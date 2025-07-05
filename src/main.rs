d3214ffe8aee88597b896741b002d10c99dbf023
Kanay Gupta <z5477443@ad.unsw.edu.au>
2025-07-05 22:01:56 +1000

    Refactored making the scene

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use rand::random;
 use ray_tracing::Colour;
 use ray_tracing::camera::Camera;
+use ray_tracing::hittable::Hittable;
 use ray_tracing::hittable_list::*;
 use ray_tracing::material::Dielectric;
 use ray_tracing::material::Lambertian;
@@ -26,6 +27,8 @@ fn main() {
     );
 
     let mut world = HittableList::new();
+
+    // Creating the small balls in the render
     for i in -10..=10 {
         for j in -10..=10 {
             let centre = Point::new(
@@ -42,90 +45,109 @@ fn main() {
             }
 
             let material = match random() {
-                0.0..0.33 => Arc::new(Box::new(Lambertian::new(Colour::new(
+                0.0..0.33 => make_material_shareable(Lambertian::new(Colour::new(
                     random(),
                     random(),
                     random(),
-                ))) as Box<dyn Material + Send + Sync>),
-                0.33..0.66 => Arc::new(Box::new(Metal::new(
+                ))),
+                0.33..0.66 => make_material_shareable(Metal::new(
                     random::<f64>() * Colour::new(random(), random(), random()),
                     random::<f64>(),
-                )) as Box<dyn Material + Send + Sync>),
-                0.66..0.90 => Arc::new(Box::new(Light::new(Colour::new(
-                    random(),
-                    random(),
-                    random(),
-                ))) as Box<dyn Material + Send + Sync>),
-                _ => Arc::new(Box::new(Dielectric::new(random::<f64>() / 2.0 + 0.75))
-                    as Box<dyn Material + Send + Sync>),
+                )),
+                0.66..0.90 => {
+                    make_material_shareable(Light::new(Colour::new(random(), random(), random())))
+                }
+                _ => make_material_shareable(Dielectric::new(random::<f64>() / 2.0 + 0.75)),
             };
 
-            world.add(Arc::new(Box::new(Sphere::new(
-                centre,
+            world.add(create_world_object(
+                centre.x,
+                centre.y,
+                centre.z,
                 0.2,
                 material.clone(),
-            ))));
+            ));
         }
     }
 
-    let material_ground = Arc::new(Box::new(Lambertian::new(0.5 * Colour::new(1.0, 1.0, 1.0)))
-        as Box<dyn Material + Send + Sync>);
-    let material_left =
-        Arc::new(Box::new(Lambertian::new(Colour::new(0.4, 0.2, 0.1)))
-            as Box<dyn Material + Send + Sync>);
-    // let material_light = Arc::new(
-    //     Box::new(Light::new(Colour::new(1.0, 1.0, 1.0))) as Box<dyn Material + Send + Sync>
-    // );
-    let material_glass =
-        Arc::new(Box::new(Dielectric::new(1.5)) as Box<dyn Material + Send + Sync>);
-    let material_bubble =
-        Arc::new(Box::new(Dielectric::new(1.0 / 1.5)) as Box<dyn Material + Send + Sync>);
-    let material_right = Arc::new(
-        Box::new(Light::new(Colour::new(1.0, 0.9, 0.4))) as Box<dyn Material + Send + Sync>
-    );
-    let material_centre =
-        Arc::new(Box::new(Metal::new(Colour::new(0.7, 0.6, 0.5), 0.0))
-            as Box<dyn Material + Send + Sync>);
+    let material_ground =
+        make_material_shareable(Lambertian::new(0.5 * Colour::new(1.0, 1.0, 1.0)));
+    let material_left = make_material_shareable(Lambertian::new(Colour::new(0.4, 0.2, 0.1)));
+    let material_glass = make_material_shareable(Dielectric::new(1.5));
+    let material_bubble = make_material_shareable(Dielectric::new(1.0 / 1.5));
+    let material_right = make_material_shareable(Light::new(Colour::new(1.0, 0.9, 0.4)));
+    let material_centre = make_material_shareable(Metal::new(Colour::new(0.7, 0.6, 0.5), 0.0));
 
-    // Arc::new(Box::new(Metal::new(Colour::new(0.8, 0.6, 0.2), 1.0)) as Box<dyn Material>);
-
-    // World
-    // let mut world = HittableList::new();
-    world.add(Arc::new(Box::new(Sphere::new(
-        Point::new(0.0, -1000.0, 0.0),
+    //  The ground
+    world.add(create_world_object(
+        0.0,
+        -1000.0,
+        0.0,
         999.99,
         material_ground.clone(),
-    ))));
-    world.add(Arc::new(Box::new(Sphere::new(
-        Point::new(-4.0, 1.0, 0.0),
+    ));
+
+    // Leftmost big ball in the render
+    world.add(create_world_object(
+        -4.0,
+        1.0,
+        0.0,
         1.0,
         material_left.clone(),
-    ))));
-    // world.add(Arc::new(Box::new(Sphere::new(
-    //     Point::new(0.0, 1000.0, 500.0),
-    //     500.0,
-    //     material_light.clone(),
-    // ))));
-    world.add(Arc::new(Box::new(Sphere::new(
-        Point::new(0.0, 1.0, 0.0),
+    ));
+
+    // Centre big ball in the render
+    world.add(create_world_object(
+        0.0,
+        1.0,
+        0.0,
         1.0,
         material_centre.clone(),
-    ))));
-    world.add(Arc::new(Box::new(Sphere::new(
-        Point::new(4.0, 1.0, 0.0),
+    ));
+
+    // Rightmost big ball in the render
+    world.add(create_world_object(
+        4.0,
         1.0,
-        material_glass.clone(),
-    ))));
-    world.add(Arc::new(Box::new(Sphere::new(
-        Point::new(4.0, 1.0, 0.0),
+        0.0,
         0.9,
         material_bubble.clone(),
-    ))));
-    world.add(Arc::new(Box::new(Sphere::new(
-        Point::new(4.0, 1.0, 0.0),
+    ));
+    world.add(create_world_object(
+        4.0,
+        1.0,
+        0.0,
+        1.0,
+        material_glass.clone(),
+    ));
+
+    world.add(create_world_object(
+        4.0,
+        1.0,
+        0.0,
         0.8,
         material_right.clone(),
-    ))));
+    ));
 
     camera.render(Arc::new(Box::new(world)), 16);
+}
+
+fn make_material_shareable(
+    material: impl Material + 'static,
+) -> Arc<Box<dyn Material + Send + Sync>> {
+    Arc::new(Box::new(material) as Box<dyn Material + Send + Sync>)
+}
+
+fn create_world_object(
+    x: f64,
+    y: f64,
+    z: f64,
+    radius: f64,
+    material: Arc<Box<dyn Material + Send + Sync>>,
+) -> Arc<Box<dyn Hittable>> {
+    Arc::new(Box::new(Sphere::new(
+        Point::new(x, y, z),
+        radius,
+        material.clone(),
+    )))
 }
